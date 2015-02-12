65d1e83633b57ef4549c220de65c793e5744cc9a
vana <rysaviva@fit.cvut.cz>
2015-02-12 10:10:24 +0100

    prvni verze

---
+++ sfe-lang.h
@@ -0,0 +1,22 @@
+#include "config.h"
+#include "system.h"
+#include "ansidecl.h"
+#include "coretypes.h"
+
+#include "diagnostic-core.h"
+#include "input.h"
+
+#include "hash-set.h"
+#include "machmode.h"
+#include "vec.h"
+#include "double-int.h"
+#include "input.h"
+#include "alias.h"
+#include "symtab.h"
+#include "options.h"
+#include "wide-int.h"
+#include "inchash.h"
+
+#include "tree.h"
+#include "fold-const.h"
+
